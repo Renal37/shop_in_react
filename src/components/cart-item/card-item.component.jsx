d5d137a4b600ca8088f59b416c71208e1f783c6c
Renal37 <renalplaycsgo@gmail.com>
2024-03-10 18:25:10 +0300

    fix style

--- src/components/cart-item/card-item.component.jsx
+++ src/components/cart-item/card-item.component.jsx
@@ -1,16 +1,18 @@
-import './card-item.style.scss'
+import './card-item.style.scss';
 
-const CartItem = ({ cartItem: { name, quantity, price, imageUrl } }) => {
-    return (
-        <div className='cart-item-container'>
-            <img src={imageUrl} alt={`${name}`} />
-            <div className="item-details">
-                <span className='name'>{name}</span>
-                <span className='price'>{quantity} x  ${price}</span>
-                </div>
-        </div>
-    )
+const CartItem = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div className='cart-item-container'>
+      <img src={imageUrl} alt={`${name}`} />
+      <div className='item-details'>
+        <span className='name'>{name}</span>
+        <span className='price'>
+          {quantity} x ${price}
+        </span>
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default CartItem
+export default CartItem;
--- src/components/category-preview/category-preview.camponent.jsx
+++ src/components/category-preview/category-preview.camponent.jsx
@@ -1,12 +1,15 @@
 import ProductCard from '../product-card/product-card.component';
 
+import { Link } from 'react-router-dom';
+
 import './category-preview.style.scss'
 
 const CategoryPreview = ({title,products})=>{
     return(
         <div className='category-preview-container'>
             <h2>
-                <span className='title'>{title.toUpperCase()}</span>
+            <Link to={title} className='title'>{title.toUpperCase()}</Link>
+                {/* <a href={`shop/${title.toLowerCase()}`} className='title'>{title.toUpperCase()}</a> */}
             </h2>
             <div className='preview'>
                 {
